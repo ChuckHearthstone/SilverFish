7e4a49e33cd04c1f68d14555cb193bba86140263
Chuck Lu <chuck.lu@qq.com>
2019-08-20 21:14:38 +0800

    add simulation for card Underbelly Angler

---
+++ SilverFish/cards/DAL/Sim_DAL_049.cs
@@ -0,0 +1,18 @@
+﻿namespace HREngine.Bots
+{
+    /// <summary>
+    /// Underbelly Angler
+    /// 下水道渔人
+    /// </summary>
+    public class Sim_DAL_049 : SimTemplate
+    {
+        public override void onCardWasPlayed(Playfield p, CardDB.Card c, bool wasOwnCard, Minion triggerEffectMinion)
+        {
+            if (wasOwnCard == triggerEffectMinion.own)
+            {
+                //Bluegill Warrior 蓝腮战士
+                p.drawACard(CardDB.cardIDEnum.CS2_173, wasOwnCard, true);
+            }
+        }
+    }
+}
