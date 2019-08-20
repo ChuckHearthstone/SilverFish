44a0fd139dae1bae410da3c04ae4f71b3c1b5f22
Chuck Lu <chuck.lu@qq.com>
2019-08-20 18:29:43 +0800

    add comments for sim of Savage Roar

--- SilverFish/penalties/Pen_CS2_011.cs
+++ SilverFish/penalties/Pen_CS2_011.cs
@@ -1,15 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace HREngine.Bots
 {
-	class Pen_CS2_011 : PenTemplate //savageroar
-	{
-
-//    verleiht euren charakteren +2 angriff in diesem zug.
-		public override int getPlayPenalty(Playfield p, Minion m, Minion target, int choice, bool isLethal)
-		{
+    /// <summary>
+    /// Savage Roar
+    /// 野蛮咆哮
+    /// </summary>
+    class Pen_CS2_011 : PenTemplate
+    {
+        /// <summary>
+        /// "LocStringEnUs": "Give your characters +2_Attack this turn.",
+        /// "LocStringZhCn": "在本回合中，使你的所有角色获得+2攻击力。",
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="m"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
+        /// <param name="isLethal"></param>
+        /// <returns></returns>
+        public override int getPlayPenalty(Playfield p, Minion m, Minion target, int choice, bool isLethal)
+        {
             if (!isLethal)
             {
                 int targets = 0;
@@ -17,20 +25,23 @@ namespace HREngine.Bots
                 {
                     if (mnn.Ready) targets++;
                 }
+
                 if (p.ownHero.Ready || p.ownHero.numAttacksThisTurn == 0) targets++;
 
                 if (targets <= 1)
                 {
                     return 40;
                 }
+
                 if (targets <= 2)
                 {
                     return 20;
                 }
-                
-            }
-            return 0;
-		}
 
-	}
+            }
+
+            return 0;
+        }
+
+    }
 }
