e1dcf5e650707c8076f9c8f451b7cc709acb1b9b
Chuck Lu <chuck.lu@qq.com>
2019-08-23 19:00:42 +0800

    add simulation for Bloodsworn Mercenary

---
+++ SilverFish/cards/ULD/Sim_ULD_720.cs
@@ -0,0 +1,27 @@
+﻿namespace HREngine.Bots
+{
+    /// <summary>
+    /// Bloodsworn Mercenary
+    /// 血誓雇佣兵
+    /// </summary>
+    public class Sim_ULD_720 : SimTemplate
+    {
+        /// <summary>
+        /// "LocStringEnUs": "[x]<b>Battlecry</b>: Choose a\ndamaged friendly minion.\nSummon a copy of it.",
+        /// "LocStringZhCn": "<b>战吼：</b>选择一个受伤的友方随从，召唤一个它的复制。",
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="own"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
+        public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
+        {
+            if (target != null && target.own && target.wounded)
+            {
+                int position = p.ownMinions.Count;
+                p.callKid(own.handcard.card, position, true);
+                p.ownMinions[position].setMinionToMinion(target);
+            }
+        }
+    }
+}
