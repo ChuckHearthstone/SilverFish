57d897c48355a3914074ab04449ec517652c8a9d
Chuck Lu <chuck.lu@qq.com>
2019-09-02 02:41:26 +0800

    add simulation for card 香蕉小丑(Banana Buffoon)

---
+++ SilverFish/cards/04Expansion/010TRL/Sim_TRL_509.cs
@@ -0,0 +1,38 @@
+﻿using HREngine.Bots;
+
+namespace SilverFish.cards._04Expansion._010TRL
+{
+    /// <summary>
+    /// Banana Buffoon
+    /// 香蕉小丑
+    /// </summary>
+    public class Sim_TRL_509 : SimTemplate
+    {
+        /// <summary>
+        /// Battlecry: Add 2 Bananas to your hand.
+        /// 战吼：将两个香蕉置入你的手牌。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="own"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
+        public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
+        {
+            int count = 2;
+            for (int i = 1; i <= count; i++)
+            {
+                p.drawACard(CardDB.cardName.bananas, own.own, true);
+                //the card draw counter will plus one in drawACard method
+                //but the card is not drawn from deck, so we subtract it back
+                if (own.own)
+                {
+                    p.owncarddraw--;
+                }
+                else
+                {
+                    p.enemycarddraw--;
+                }
+            }
+        }
+    }
+}
