545164f2a16ca3d67792a78ce7a0ea7bc4ee7c91
Chuck Lu <chuck.lu@qq.com>
2019-09-01 14:21:07 +0800

    override the content of notImplementedCardSimulation.log

--- SilverFish/Helpers/LogHelper.cs
+++ SilverFish/Helpers/LogHelper.cs
@@ -10,7 +10,7 @@ namespace SilverFish.Helpers
 
         private static string MainLogFileName { get; set; }
 
-        private static void WriteLog(object obj, string fileName, string subfolder = "")
+        private static void AppendText(object obj, string fileName, string subfolder = "")
         {
             var folder = Path.Combine(Settings.Instance.BaseDirectory, LogFolder);
             if (!string.IsNullOrWhiteSpace(subfolder))
@@ -24,28 +24,55 @@ namespace SilverFish.Helpers
                 FileHelper.CreateEmptyFile(filePath);
             }
 
+            AppendText(filePath, obj);
+        }
+
+        private static void AppendText(string filePath, object obj)
+        {
             using (StreamWriter sw = File.AppendText(filePath))
             {
                 sw.WriteLine(obj?.ToString());
             }
         }
 
+        private static void WriteAllText(string filePath, object obj)
+        {
+            File.WriteAllText(filePath, obj?.ToString());
+        }
+
+        private static void WriteAllText(object obj, string fileName, string subfolder)
+        {
+            var folder = Path.Combine(Settings.Instance.BaseDirectory, LogFolder);
+            if (!string.IsNullOrWhiteSpace(subfolder))
+            {
+                folder = Path.Combine(folder, subfolder);
+            }
+
+            var filePath = Path.Combine(folder, fileName);
+            if (!File.Exists(filePath))
+            {
+                FileHelper.CreateEmptyFile(filePath);
+            }
+
+            WriteAllText(filePath, obj);
+        }
+
         public static void WriteNotImplementedCardSimulationLog(string content)
         {
-            WriteLog(content, "notImplementedCardSimulation.log");
+            WriteAllText(content, "notImplementedCardSimulation.log", string.Empty);
         }
 
         public static string CombatLogFileName { get; set; } = "Combat.log";
 
         public static void WriteCombatLog(object obj)
         {
-            WriteLog(obj, CombatLogFileName, "CombatLogs");
+            AppendText(obj, CombatLogFileName, "CombatLogs");
         }
 
         public static void WriteMainLog(object obj)
         {
             MainLogFileName = $"SilverFish{DateTime.Now:yyyyMMdd}.log";
-            WriteLog(obj, MainLogFileName);
+            AppendText(obj, MainLogFileName);
         }
     }
 }
