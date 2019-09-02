6cc12fb9dbf86ad5a507759b50421d521acd3207
wayne0036 <52026084+wayne0036@users.noreply.github.com>
2019-09-02 18:06:02 +0800

    add GIL cards

---
+++ SilverFish/cards/04Expansion/008GIL/Sim_GIL_828.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREngine.Bots
+{
+	class Sim_GIL_828 : SimTemplate //* 凶猛狂暴
+	{
+		//使一个野兽获得+3/+3。将它的三张复制洗入你的牌库，且这些复制都具有+3/+3。
+        //未添加洗入牌库特效
+
+        public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
+        {
+            p.minionGetBuffed(target, 3, 3);
+        }
+	}
+}
