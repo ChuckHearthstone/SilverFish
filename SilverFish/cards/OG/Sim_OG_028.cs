cf590a54e5ea5e7e659e759f3e6107bcdbd45e95
Chuck Lu <chuck.lu@qq.com>
2019-09-02 21:58:10 +0800

    add card simulation for 深渊魔物(Thing from Below)

---
+++ SilverFish/cards/OG/Sim_OG_028.cs
@@ -0,0 +1,16 @@
+﻿using HREngine.Bots;
+
+namespace SilverFish.cards.OG
+{
+    /// <summary>
+    /// Thing from Below
+    /// 深渊魔物
+    /// </summary>
+    public class Sim_OG_028 : SimTemplate
+    {
+        //Taunt Costs 1 less for each Totem you've summoned this game.
+        //嘲讽 在本局对战中，你每召唤一个图腾，该牌的法力值消耗便减少1点。
+        //Simulation for this card is implemented in method getManaCost of Card class 
+    }
+
+}
