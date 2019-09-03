23f716de2719e492cc99ed42bd4e472484d5b114
Chuck Lu <chuck.lu@qq.com>
2019-09-04 00:01:52 +0800

    add simulation for card 青玉魔像(Jade Golem)

---
+++ SilverFish/cards/04Expansion/004CFM/Sim_CFM_712.cs
@@ -0,0 +1,127 @@
+﻿using HREngine.Bots;
+
+namespace SilverFish.cards._04Expansion._004CFM
+{
+    //Jade Golem
+    //青玉魔像
+
+    public class Sim_CFM_712_t01 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t02 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t03 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t04 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t05 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t06 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t07 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t08 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t09 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t10 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t11 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t12 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t13 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t14 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t15 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t16 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t17 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t18 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t19 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t20 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t21 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t22 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t23 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t24 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t25 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t26 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t27 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t28 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t29 : SimTemplate
+    {
+    }
+
+    public class Sim_CFM_712_t30 : SimTemplate
+    {
+    }
+}
