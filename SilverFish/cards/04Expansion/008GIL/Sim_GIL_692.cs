8fc1da38d09f283d00f7f8c575572f436e8bde0f
Chuck Lu <chuck.lu@qq.com>
2019-09-04 00:06:33 +0800

    add simulation for card 吉恩·格雷迈恩(Genn Greymane)

---
+++ SilverFish/cards/04Expansion/008GIL/Sim_GIL_692.cs
@@ -0,0 +1,15 @@
+﻿using HREngine.Bots;
+
+namespace SilverFish.cards._04Expansion._008GIL
+{
+    /// <summary>
+    /// Genn Greymane
+    /// 吉恩·格雷迈恩
+    /// </summary>
+    public class Sim_GIL_692 : SimTemplate
+    {
+        //Start of Game: If your deck has only even- Cost cards, your starting Hero Power costs 1.
+        //对战开始时：如果你的套牌中只有法力值消耗为偶数的牌，你的初始英雄技能的法力值消耗变为1。
+        //Simulation for this card is implemented through EvenDeckHelper class 
+    }
+}
