7f84d249648d004a2d94bb86a05296a0c6c6b4ab
jianjam <jianjam@qq.com>
2019-09-04 11:58:27 +0800

    Modify effect for SIM_GIL_553 card. to be verified.

--- SilverFish/cards/04Expansion/008GIL/Sim_GIL_553.cs
+++ SilverFish/cards/04Expansion/008GIL/Sim_GIL_553.cs
@@ -2,23 +2,30 @@ using HREngine.Bots;
 
 namespace SilverFish.cards._04Expansion._008GIL
 {
-    class Sim_GIL_553 : SimTemplate //精灵之森
+    /// <summary>
+    /// Wispering Woods
+    /// 精灵之森
+    /// </summary>
+    class Sim_GIL_553 : SimTemplate
     {
-
-        //   你每有一张手牌，便召唤一个1/1的小精灵。
-        
-        CardDB.Card kid = CardDB.Instance.getCardDataFromID(CardDB.cardIDEnum.CS2_231); //小精灵
-
+        /// <summary>
+        /// Summon a 1/1 Wisp for each card in your hand.
+        /// 你每有一张手牌，便召唤一个1/1的小精灵。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="own"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
         public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
         {
-            
+            CardDB.Card kid = CardDB.Instance.getCardDataFromID(CardDB.cardIDEnum.CS2_231); //小精灵
             int posi = (ownplay) ? p.ownMinions.Count : p.enemyMinions.Count;
-            p.CallKid(kid, posi, ownplay);
-            posi++;
-            p.CallKid(kid, posi, ownplay);
+            int wispNum = Math.min(7 - posi, (ownplay) ? p.owncards.Count : p.enemyAnzCards);
+            for (int i = 0; i < wiapNum; i++)
+            {
+                p.CallKid(kid, posi, ownplay);
+                posi++;
+            }
         }
-
-
     }
-
 }
