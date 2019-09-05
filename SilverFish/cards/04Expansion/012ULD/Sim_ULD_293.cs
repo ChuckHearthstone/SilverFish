c5007a09c18774c565615e636baa3ffb8a769807
Chuck Lu <chuck.lu@qq.com>
2019-09-05 13:18:55 +0800

    add simulation for card 云雾王子(Cloud Prince)

---
+++ SilverFish/cards/04Expansion/012ULD/Sim_ULD_293.cs
@@ -0,0 +1,26 @@
+﻿namespace HREngine.Bots
+{
+    /// <summary>
+    /// Cloud Prince
+    /// 云雾王子
+    /// </summary>
+    public class Sim_ULD_293 : SimTemplate
+    {
+        /// <summary>
+        /// Battlecry: If you control a Secret, deal 6 damage.
+        /// 战吼： 如果你控制一个奥秘，则造成6点伤害。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="own"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
+        public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
+        {
+            var damage = 6;
+            if (p.ownSecretsIDList.Count >= 1)
+            {
+                p.minionGetDamageOrHeal(target, damage);
+            }
+        }
+    }
+}
