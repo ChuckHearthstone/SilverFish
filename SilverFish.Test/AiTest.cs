a375565423d16447b97a90da7acbe039cc52b6e1
Chuck Lu <chuck.lu@qq.com>
2019-09-06 19:25:44 +0800

    add unit test project

---
+++ SilverFish.Test/AiTest.cs
@@ -0,0 +1,30 @@
+﻿using HREngine.Bots;
+using NUnit.Framework;
+
+namespace SilverFish.Test
+{
+    [TestFixture]
+    public class AiTest
+    {
+        [Test]
+        public void Test()
+        {
+            Settings.Instance.LogFolderPath = @"C:\repository\GitHub\ChuckLu\Test\HearthbuddyRelease\Logs";
+            Settings.Instance.DataFolderPath =
+                @"C:\repository\GitHub\ChuckLu\Test\HearthbuddyRelease\Routines\DefaultRoutine\SilverFish\Data\";
+            Settings.Instance.BaseDirectory = @"C:\repository\GitHub\ChuckLu\Test\HearthbuddyRelease";
+            Settings.Instance.writeToSingleFile = true;//C:\repository\GitHub\ChuckLu\Test\HearthbuddyRelease\Logs\SilverFish.log
+            if (Hrtprozis.Instance.settings == null)
+            {
+                Hrtprozis.Instance.setInstances();
+                ComboBreaker.Instance.setInstances();
+                PenalityManager.Instance.setInstances();
+            }
+
+            //-mode: 0-all, 1-lethalcheck, 2-normal
+            Ai ai = Ai.Instance;
+            ai.autoTester(true, string.Empty, 0);
+
+        }
+    }
+}
