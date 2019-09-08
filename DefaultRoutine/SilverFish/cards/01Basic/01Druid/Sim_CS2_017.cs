39e6325109a758184204e28ff98c2a23d99d94cc
Chuck Lu <chuck.lu@qq.com>
2019-09-08 17:00:28 +0800

    refactor code

--- DefaultRoutine/SilverFish/cards/01Basic/01Druid/Sim_CS2_017.cs
+++ DefaultRoutine/SilverFish/cards/01Basic/01Druid/Sim_CS2_017.cs
@@ -2,24 +2,25 @@ using HREngine.Bots;
 
 namespace SilverFish.cards._01Basic._01Druid
 {
-	class Sim_CS2_017 : SimTemplate //shapeshift
+    /// <summary>
+    /// Shapeshift
+    /// 变形
+    /// </summary>
+	public class Sim_CS2_017 : SimTemplate
 	{
-
-//    heldenfähigkeit/\n+1 angriff in diesem zug.\n+1 rüstung.
-
+        /// <summary>
+        /// Hero Power +1 Attack this turn. +1 Armor.
+        /// 英雄技能 本回合+1攻击力。  +1护甲值。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="ownplay"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
         public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
         {
-            if (ownplay)
-            {
-                p.minionGetTempBuff(p.ownHero, 1, 0);
-                p.minionGetArmor(p.ownHero,1);
-            }
-            else
-            {
-                p.minionGetTempBuff(p.enemyHero, 1, 0);
-                p.minionGetArmor(p.enemyHero,1);
-            }
+            var hero = ownplay ? p.ownHero : p.enemyHero;
+            p.minionGetTempBuff(hero, 1, 0);
+            p.minionGetArmor(hero, 1);
         }
-
-	}
+    }
 }
--- DefaultRoutine/SilverFish/cards/01Basic/02Hunter/Sim_DS1h_292.cs
+++ DefaultRoutine/SilverFish/cards/01Basic/02Hunter/Sim_DS1h_292.cs
@@ -2,39 +2,47 @@ using HREngine.Bots;
 
 namespace SilverFish.cards._01Basic._02Hunter
 {
-	class Sim_DS1h_292 : SimTemplate //* Steady Shot
-	{
-		//Hero Power: Deal 2 damage to the enemy hero.
-		
-		public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
+    /// <summary>
+    /// Steady Shot
+    /// 稳固射击
+    /// </summary>
+	public class Sim_DS1h_292 : SimTemplate
+    {
+        protected virtual int Damage { get; set; } = 2;
+
+        /// <summary>
+        /// Hero Power Deal 2 damage.
+        /// 英雄技能 造成2点伤害。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="ownplay"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
+        public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
         {
-            int dmg = (ownplay) ? p.getHeroPowerDamage(2) : p.getEnemyHeroPowerDamage(2);
-            if (target == null) target = ownplay ? p.enemyHero : p.ownHero;
+            int dmg = ownplay ? p.getHeroPowerDamage(Damage) : p.getEnemyHeroPowerDamage(Damage);
+            if (target == null)
+            {
+                target = ownplay ? p.enemyHero : p.ownHero;
+            }
             p.minionGetDamageOrHeal(target, dmg);
 		}
 	}
 
-    class Sim_DS1h_292_H1 : SimTemplate //* Steady Shot
+    public class Sim_DS1h_292_H1 : Sim_DS1h_292
     {
-        //Hero Power: Deal 2 damage to the enemy hero.
-
-        public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
-        {
-            int dmg = (ownplay) ? p.getHeroPowerDamage(2) : p.getEnemyHeroPowerDamage(2);
-            if (target == null) target = ownplay ? p.enemyHero : p.ownHero;
-            p.minionGetDamageOrHeal(target, dmg);
-        }
     }
 
-    class Sim_DS1h_292_H1_AT_132 : SimTemplate //* Ballista Shot
+    /// <summary>
+    /// Ballista Shot
+    /// 弩炮射击
+    /// </summary>
+    public class Sim_DS1h_292_H1_AT_132 : Sim_DS1h_292
     {
-        //Hero Power: Deal 3 damage to the enemy hero.
-
-        public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
-        {
-            int dmg = (ownplay) ? p.getHeroPowerDamage(3) : p.getEnemyHeroPowerDamage(3);
-            if (target == null) target = ownplay ? p.enemyHero : p.ownHero;
-            p.minionGetDamageOrHeal(target, dmg);
-        }
+        /// <summary>
+        /// Hero Power Deal 3 damage.
+        /// 英雄技能 造成3点伤害。
+        /// </summary>
+        protected override int Damage { get; set; } = 3;
     }
 }
