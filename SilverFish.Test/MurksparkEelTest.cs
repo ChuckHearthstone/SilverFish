5282729b9b08cdd34572db4be06b99f9cb6a0c7a
Chuck Lu <chuck.lu@qq.com>
2019-09-10 19:32:27 +0800

    add test for 阴燃电鳗(Murkspark Eel)

---
+++ SilverFish.Test/MurksparkEelTest.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using HREngine.Bots;
+using NUnit.Framework;
+
+namespace SilverFish.Test
+{
+    [TestFixture]
+    class MurksparkEelTest : TestBase
+    {
+        [Test]
+        public void Test()
+        {
+
+            var testFilePath = Path.Combine(Settings.Instance.BaseDirectory,
+                @"SilverFish.Test\Data\MurksparkEelTest.txt");
+            var data = File.ReadAllText(testFilePath);
+            //Console.WriteLine(data);
+
+            Ai ai = Ai.Instance;
+            ai.autoTester(true, data, 0);
+
+        }
+    }
+}
