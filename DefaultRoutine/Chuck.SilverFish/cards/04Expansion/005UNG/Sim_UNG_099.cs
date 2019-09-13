c74a2f58096f37549290081270fd177375ff8324
Chuck Lu <chuck.lu@qq.com>
2019-09-13 15:42:01 +0800

    add comments for 狂奔的魔暴龙(Charged Devilsaur)

--- DefaultRoutine/Chuck.SilverFish/cards/04Expansion/005UNG/Sim_UNG_099.cs
+++ DefaultRoutine/Chuck.SilverFish/cards/04Expansion/005UNG/Sim_UNG_099.cs
@@ -2,18 +2,31 @@ using Chuck.SilverFish;
 
 namespace SilverFish.cards._04Expansion._005UNG
 {
-	class Sim_UNG_099 : SimTemplate //* Charged Devilsaur
+    /// <summary>
+    /// Charged Devilsaur
+    /// 狂奔的魔暴龙
+    /// </summary>
+    class Sim_UNG_099 : SimTemplate
 	{
-		//Charge Battlecry: Can't attack heroes this turn.
-
-		public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
+        /// <summary>
+        /// Charge Battlecry: Can't attack heroes this turn.
+        /// 冲锋，战吼：在本回合中无法攻击英雄。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="own"></param>
+        /// <param name="target"></param>
+        /// <param name="choice"></param>
+        public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
 		{
             own.cantAttackHeroes = true;
 		}
 
         public override void onTurnStartTrigger(Playfield p, Minion triggerEffectMinion, bool turnStartOfOwner)
         {
-            if (triggerEffectMinion.own == turnStartOfOwner) triggerEffectMinion.cantAttackHeroes = false;
+            if (triggerEffectMinion.own == turnStartOfOwner)
+            {
+                triggerEffectMinion.cantAttackHeroes = false;
+            }
         }
     }
 }
