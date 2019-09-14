e8c05d18bc44ba5a6459691a3d63e2e766588f9c
Chuck Lu <chuck.lu@qq.com>
2019-09-14 10:54:58 +0800

    refactor code

--- DefaultRoutine/Chuck.SilverFish/cards/04Expansion/011DAL/SIm_DAL_606.cs
+++ DefaultRoutine/Chuck.SilverFish/cards/04Expansion/011DAL/SIm_DAL_606.cs
@@ -1,7 +1,6 @@
 using Chuck.SilverFish;
 using SilverFish.Helpers;
 
-
 namespace SilverFish.cards._04Expansion._011DAL
 {
     /// <summary>
@@ -20,11 +19,11 @@ namespace SilverFish.cards._04Expansion._011DAL
         /// <param name="choice"></param>
         public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
         {
-            if (target != null)
+            p.minionGetDestroyed(target);
+            var count = 2;
+            for (var i = 1; i <= count; i++)
             {
-                p.minionGetDestroyed(target);
                 var cardIdEnum = LackeyHelper.Instance.GetRandomLackey();
-                p.drawACard(cardIdEnum, own.own, true);
                 p.drawACard(cardIdEnum, own.own, true);
             }
         }
