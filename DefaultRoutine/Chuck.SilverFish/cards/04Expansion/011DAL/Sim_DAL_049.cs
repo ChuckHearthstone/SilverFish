e964f9e0dbb992926e722bcb27173053270ee7d1
Chuck Lu <chuck.lu@qq.com>
2019-09-26 19:57:48 +0800

    fix simulation for 下水道渔人(Underbelly Angler)

--- DefaultRoutine/Chuck.SilverFish/cards/04Expansion/011DAL/Sim_DAL_049.cs
+++ DefaultRoutine/Chuck.SilverFish/cards/04Expansion/011DAL/Sim_DAL_049.cs
@@ -9,12 +9,20 @@ namespace SilverFish.cards._04Expansion._011DAL
     /// </summary>
     public class Sim_DAL_049 : SimTemplate
     {
-        public override void onCardWasPlayed(Playfield p, CardDB.Card c, bool wasOwnCard, Minion triggerEffectMinion)
+        /// <summary>
+        /// After you play a Murloc, add a random Murloc to your hand.
+        /// 在你使用一张鱼人牌后，随机将一张鱼人牌置入你的手牌。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="hc"></param>
+        /// <param name="wasOwnCard"></param>
+        /// <param name="triggerEffectMinion"></param>
+        public override void onCardIsGoingToBePlayed(Playfield p, Handmanager.Handcard hc, bool wasOwnCard, Minion triggerEffectMinion)
         {
-            if (wasOwnCard == triggerEffectMinion.own)
+            if (triggerEffectMinion.own == wasOwnCard)
             {
-                int murlocEnumValue = (int) TAG_RACE.MURLOC;
-                if (triggerEffectMinion.handcard.card.race == murlocEnumValue)
+                int murlocEnumValue = (int)TAG_RACE.MURLOC;
+                if (hc.card.race == murlocEnumValue)
                 {
                     //Bluegill Warrior 蓝腮战士
                     p.drawACard(CardIdEnum.CS2_173, wasOwnCard, true);
