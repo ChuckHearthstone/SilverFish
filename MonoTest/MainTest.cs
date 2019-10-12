ccead8314095a0379656ef7cbb142b541ffb3b96
Chuck Lu <chuck.lu@qq.com>
2019-10-12 12:54:10 +0800

    generate code for bot class276

--- MonoTest/MainTest.cs
+++ MonoTest/MainTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net;
 using NUnit.Framework;
 
 namespace MonoTest
@@ -15,6 +14,8 @@ namespace MonoTest
             var addressFilePath = @"C:\repository\GitHub\ChuckLu\HearthBuddy\SilverFish\MonoTest\mono-address.txt";
             var lines = File.ReadAllLines(addressFilePath);
             string separator = " - ";
+
+            //key is function name, and value should be address
             Dictionary<string, string> dictionary = new Dictionary<string, string>();
             foreach (var line in lines)
             {
@@ -30,7 +31,23 @@ namespace MonoTest
 
             foreach (var item in dictionary)
             {
-                Console.WriteLine($"{item.Key}={item.Value}");
+                //Console.WriteLine($"{item.Key}={item.Value}");
+            }
+
+            var functionDictionary = MonoFunctions.Instance.FunctionDictionary;
+            //this.intptr_1 = this.intptr_0 + 102855;
+            string format = "intptr_{0} = intptr_0 + 0x{1};";
+            foreach (var item in functionDictionary) //key is id and value is function name
+            {
+                var functionName = item.Value;
+                var ptrId = item.Key;
+                if (!dictionary.ContainsKey(functionName))
+                {
+                    throw new Exception($"Can not find address for function {functionName}");
+                }
+
+                var address = dictionary[functionName];
+                Console.WriteLine(format, ptrId, address);
             }
         }
     }
--- MonoTest/MonoFunctions.cs
+++ MonoTest/MonoFunctions.cs
@@ -11,49 +11,49 @@ namespace MonoTest
             Init();
         }
 
-        public Dictionary<int, string> Dictionary = new Dictionary<int, string>();
+        public Dictionary<int, string> FunctionDictionary = new Dictionary<int, string>();
 
         private void Init()
         {
-            Dictionary.Add(1, "mono_class_get");
-            Dictionary.Add(2, "mono_class_get_name");
-            Dictionary.Add(3, " mono_class_get_namespace");
-            Dictionary.Add(4, "mono_assembly_get_image");
-            Dictionary.Add(5, "mono_class_get_nested_types");
-            Dictionary.Add(6, "mono_assembly_open");
-            Dictionary.Add(7, "mono_assembly_load_from_full");
-            Dictionary.Add(8, "mono_class_get_fields");
-            Dictionary.Add(9, "mono_domain_get");
-            Dictionary.Add(10, "mono_gchandle_free");
-            Dictionary.Add(11, "mono_field_get_value_object");
-            Dictionary.Add(12, "mono_gchandle_new");
-            Dictionary.Add(13, "mono_image_open_from_data_with_name");
-            Dictionary.Add(14, "mono_image_get_table_rows");
-            Dictionary.Add(15, "mono_method_signature");
-            Dictionary.Add(16, "mono_class_get_type");
-            Dictionary.Add(17, "mono_thread_interruption_checkpoint");
-            Dictionary.Add(18, "mono_thread_detach");
-            Dictionary.Add(19, "mono_class_get_methods");
+            FunctionDictionary.Add(1, "mono_class_get");
+            FunctionDictionary.Add(2, "mono_class_get_name");
+            FunctionDictionary.Add(3, "mono_class_get_namespace");
+            FunctionDictionary.Add(4, "mono_assembly_get_image");
+            FunctionDictionary.Add(5, "mono_class_get_nested_types");
+            FunctionDictionary.Add(6, "mono_assembly_open");
+            FunctionDictionary.Add(7, "mono_assembly_load_from_full");
+            FunctionDictionary.Add(8, "mono_class_get_fields");
+            FunctionDictionary.Add(9, "mono_domain_get");
+            FunctionDictionary.Add(10, "mono_gchandle_free");
+            FunctionDictionary.Add(11, "mono_field_get_value_object");
+            FunctionDictionary.Add(12, "mono_gchandle_new");
+            FunctionDictionary.Add(13, "mono_image_open_from_data_with_name");
+            FunctionDictionary.Add(14, "mono_image_get_table_rows");
+            FunctionDictionary.Add(15, "mono_method_signature");
+            FunctionDictionary.Add(16, "mono_class_get_type");
+            FunctionDictionary.Add(17, "mono_thread_interruption_checkpoint");
+            FunctionDictionary.Add(18, "mono_thread_detach");
+            FunctionDictionary.Add(19, "mono_class_get_methods");
             //mono_class_get_nesting_type 还有另外1个 mono_image_get_guid
-            Dictionary.Add(20, "mono_class_get_nesting_type");
+            FunctionDictionary.Add(20, "mono_class_get_nesting_type");
             //mono_event_get_raise_method 还有另外3个
-            Dictionary.Add(21, "mono_event_get_raise_method");
-            Dictionary.Add(22, "mono_object_unbox");
-            Dictionary.Add(23, "mono_type_get_object");
-            Dictionary.Add(24, "mono_object_get_class");
-            Dictionary.Add(25, "mono_string_new");
-            Dictionary.Add(26, "mono_runtime_invoke");
-            Dictionary.Add(27, "mono_thread_current");
-            Dictionary.Add(28, "mono_class_get_parent");
-            Dictionary.Add(29, "mono_get_root_domain");
+            FunctionDictionary.Add(21, "mono_event_get_raise_method");
+            FunctionDictionary.Add(22, "mono_object_unbox");
+            FunctionDictionary.Add(23, "mono_type_get_object");
+            FunctionDictionary.Add(24, "mono_object_get_class");
+            FunctionDictionary.Add(25, "mono_string_new");
+            FunctionDictionary.Add(26, "mono_runtime_invoke");
+            FunctionDictionary.Add(27, "mono_thread_current");
+            FunctionDictionary.Add(28, "mono_class_get_parent");
+            FunctionDictionary.Add(29, "mono_get_root_domain");
             //mono_event_get_name 还有另外3个
-            Dictionary.Add(30, "mono_event_get_name");
-            Dictionary.Add(31, "mono_thread_attach");
-            Dictionary.Add(32, "mono_string_to_utf8");
-            Dictionary.Add(33, "mono_thread_get_main");
-            Dictionary.Add(34, "mono_array_addr_with_size");
-            Dictionary.Add(35, "mono_array_element_size");
-            Dictionary.Add(36, "mono_object_get_virtual_method");
+            FunctionDictionary.Add(30, "mono_event_get_name");
+            FunctionDictionary.Add(31, "mono_thread_attach");
+            FunctionDictionary.Add(32, "mono_string_to_utf8");
+            FunctionDictionary.Add(33, "mono_thread_get_main");
+            FunctionDictionary.Add(34, "mono_array_addr_with_size");
+            FunctionDictionary.Add(35, "mono_array_element_size");
+            FunctionDictionary.Add(36, "mono_object_get_virtual_method");
         }
     }
 }
